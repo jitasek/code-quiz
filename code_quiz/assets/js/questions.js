91aa541fe40f56dee003f44fe379febce3bb0ac1
jitasek <jitka.moreira@gmail.com>
2021-10-13 15:27:01 +0200

    add questions.js + navbar with styles

---
+++ code_quiz/assets/js/questions.js
@@ -0,0 +1,27 @@
+// this file stores all the quiz questions in one object
+const questions = [
+  {
+    title: "What is the text of Question 1?",
+    code: "",
+    choices: ["text 1", "text 2", "text 3", "text 4"],
+    answer: "text 1",
+  },
+  {
+    title: "What is the text of Question 2?",
+    code: "",
+    choices: ["text 1", "text 2", "text 3", "text 4"],
+    answer: "text 2",
+  },
+  {
+    title: "What is the text of Question 3?",
+    code: "",
+    choices: ["text 1", "text 2", "text 3", "text 4"],
+    answer: "text 3",
+  },
+  {
+    title: "What is the text of Question 4?",
+    code: "",
+    choices: ["text 1", "text 2", "text 3", "text 4"],
+    answer: "text 4",
+  },
+];
